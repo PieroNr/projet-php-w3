f43a50bd292a9bab5df91d6db5dbf94ee219515f
Tristan Seclet <tristanseclet@gmail.com>
2021-12-01 12:18:47 +0100

    added styles && users.php

---
+++ users.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Utilisateurs</title>
+        <link rel="stylesheet" href="assets/css/app.css">
+    </head>
+    <body>
+        <!--        HEADER          -->
+        <div class="container">
+            <table class="w-100 table">
+                <thead>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Prénom</th>
+                        <th>Mail</th>
+                        <th>Rôle</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Seclet</td>
+                        <td>Tristan</td>
+                        <td><a href="mailto:tristanseclet@icloud.com">tristanseclet@icloud.com</a></td>
+                        <td>
+                            <label>
+                                <input type="checkbox">
+                            </label>Administrateur ?
+                        </td>
+                        <td>
+                            <button class="btn bg-danger">Supprimer l'utilisateur</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Bouteiller</td>
+                        <td>Adrien</td>
+                        <td><a href="mailto:tristanseclet@icloud.com">adrienbouteiller@gmail.com</a></td>
+                        <td>
+                            <label>
+                                <input type="checkbox">
+                            </label>Administrateur ?
+                        </td>
+                        <td>
+                            <button class="btn bg-danger">Supprimer l'utilisateur</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Neri</td>
+                        <td>Piero</td>
+                        <td><a href="mailto:tristanseclet@icloud.com">pieroneri@gmail.com</a></td>
+                        <td>
+                            <label>
+                                <input type="checkbox">
+                            </label>Administrateur ?
+                        </td>
+                        <td>
+                            <button class="btn bg-danger">Supprimer l'utilisateur</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
+<?php
